d6dd58a9896b33f574353ca2affc3289bd19c119
Nuyuzin <lentyuazon@gmail.com>
2026-06-26 09:19:28 -0300

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-const { serveHTTP } = require("stremio-addon-sdk");
-const addonInterface = require("./index");
-
-serveHTTP(addonInterface, { port: process.env.PORT || 7000 });
-console.log(`Addon rodando em http://localhost:${process.env.PORT || 7000}/manifest.json`);
